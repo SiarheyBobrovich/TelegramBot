8591c550567ebd6a2fc704d783760da65409cab6
Sergey Bobrovich <siarhey1987@gmail.com>
2022-08-16 01:28:04 +0300

    Added Keyboards

--- src/main/java/by/bobrovich/telegram/bot/keyboard/query/CityInlineKeyboardMarkup.java
+++ src/main/java/by/bobrovich/telegram/bot/keyboard/query/CityInlineKeyboardMarkup.java
@@ -1,20 +1,22 @@
 package by.bobrovich.telegram.bot.keyboard.query;
 
-import by.bobrovich.telegram.bot.keyboard.menu.KeyboardKeyName;
 import by.bobrovich.telegram.bot.utils.YamlPropertySourceFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
-@ConfigurationProperties(prefix = "yaml")
-@PropertySource(value = "classpath:cities.yml", factory = YamlPropertySourceFactory.class)
+@PropertySource(value = "classpath:/inline_keyboards/cities.yml", factory = YamlPropertySourceFactory.class)
 public class CityInlineKeyboardMarkup extends NamedInlineKeyboardMarkup {
 
-    public CityInlineKeyboardMarkup(@Value("#{'${city}'.split(' ')}") List<String> cities) {
-        super(KeyboardKeyName.CITY, cities);
+    public CityInlineKeyboardMarkup(@Value("#{'${city}'.split(';')}") List<String> cities) {
+        super("Город", cities);
+    }
+
+    @Override
+    public String getMessage() {
+        return "Выберите город для сохранения: ";
     }
 }
--- src/main/java/by/bobrovich/telegram/bot/keyboard/query/CurrencyInlineKeyboardMarkup.java
+++ src/main/java/by/bobrovich/telegram/bot/keyboard/query/CurrencyInlineKeyboardMarkup.java
@@ -1,21 +1,22 @@
 package by.bobrovich.telegram.bot.keyboard.query;
 
-import by.bobrovich.telegram.bot.keyboard.menu.KeyboardKeyName;
 import by.bobrovich.telegram.bot.utils.YamlPropertySourceFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
-@ConfigurationProperties(prefix = "yaml")
-@PropertySource(value = "classpath:currencies.yml", factory = YamlPropertySourceFactory.class)
+@PropertySource(value = "classpath:/inline_keyboards/currencies.yml", factory = YamlPropertySourceFactory.class)
 public class CurrencyInlineKeyboardMarkup extends NamedInlineKeyboardMarkup {
 
     public CurrencyInlineKeyboardMarkup(
             @Value("#{'${currency}'.split(';')}") List<String> currencies) {
-        super(KeyboardKeyName. CURRENCY,currencies);
+        super("Валюта", currencies);
+    }
+
+    public String getMessage() {
+        return "Выберите валюту и операцию: ";
     }
 }
